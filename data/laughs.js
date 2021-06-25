64a020f48f4c06f8ad9fffdbba10bd6116bdae97
Nolan Reed <jasonnolanreed@gmail.com>
2021-06-25 17:51:06 -0500

    Fix image sources to use relative path

--- data/laughs.js
+++ data/laughs.js
@@ -8,31 +8,31 @@ export default {
 	"pride-comedy-explicit": {
 		"name": "Gay Pride (Explicit) - Little Rock, AR",
 		"date": "June 30, 2017",
-		"image": "/cdn/images/laughs/thumb-lolshow.jpg",
+		"image": "cdn/images/laughs/thumb-lolshow.jpg",
 		"youtubeVideoCode": "AvojH-DQkeo"
 	},
 	"outmemphis-fundraiser": {
 		"name": "OUTMemphis Fundraiser - Memphis, TN",
 		"date": "June 30, 2016",
-		"image": "/cdn/images/laughs/outloud-raiser.png",
+		"image": "cdn/images/laughs/outloud-raiser.png",
 		"youtubeVideoCode": "9sh_Hf1aPx4"
 	},
 	"veterans-of-comedy": {
 		"name": "Veterans of Comedy - Little Rock, AR",
 		"date": "January 10, 2018",
-		"image": "/cdn/images/laughs/loonybin.png",
+		"image": "cdn/images/laughs/loonybin.png",
 		"youtubeVideoCode": "nkfEKH6dubk"
 	},
 	"cupcake-cabaret": {
 		"name": "Cupcake Cabaret - Chicago, IL",
 		"date": "April 25, 2018",
-		"image": "/cdn/images/laughs/thumb-mglcc-2.jpg",
+		"image": "cdn/images/laughs/thumb-mglcc-2.jpg",
 		"youtubeVideoCode": "3O6EBgGMyhY"
 	},
 	"fall-for-you-piano-bit-featuring-el-throb": {
 		"name": "Fall For You Piano Bit f/ El Throbv",
 		"date": "August 1, 2015",
-		"image": "/cdn/images/laughs/fall4u.png",
+		"image": "cdn/images/laughs/fall4u.png",
 		"youtubeVideoCode": "la2csXGxWmo"
 	}
 };
