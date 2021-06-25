161e94c6be34a94bbe143f5896026d0bf89d38f4
Nolan Reed <jasonnolanreed@gmail.com>
2021-06-25 16:57:36 -0500

    Complete contact page

--- pages/contact/contact-templates.js
+++ pages/contact/contact-templates.js
@@ -2,10 +2,13 @@ const makeCss = (component) => ``;
 
 const makeHtml = (component) => (
 `
-<h1 class="page-header"><jje-icon name="alternate_email"></jje-icon>Contact</h1>`
+<h1 class="page-header"><jje-icon name="alternate_email"></jje-icon>${component.pageData.header}</h1>
+<hr>
+<p>${component.pageData.description}</p>
+<jje-social></jje-social>
+`
 );
 
 export const makeTemplate = (component) => {
 	return makeCss(component) + makeHtml(component);
 };
-	
--- pages/contact/contact.js
+++ pages/contact/contact.js
@@ -1,9 +1,12 @@
 import {JJEElement} from '../../classes/jje-element.js';
 import {makeTemplate} from './contact-templates.js';
 
+import pageData from '../../data/page-data.js';
+
 export class Contact extends JJEElement {
 	constructor() {
 		super();
+		this.pageData = pageData.contact;
 	}
 
 	connectedCallback() {
@@ -18,6 +21,7 @@ export class Contact extends JJEElement {
 	render() {
 		super.render();
 		try {
+			document.title = this.pageData.title;
 			this.innerHTML = makeTemplate(this);
 		} catch(error) {
 			console.error(`Error rendering`, error);
